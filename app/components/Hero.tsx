ac42e8abbf9b8f22d2e4e6232372990e9ae60beb
Megan Byers <megan@designelixir.studio>
2025-03-27 12:22:44 -0600

    remove border

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Hero(){
     return(
         <div className="hero-container flex-end-start ">
-            <div className="hero-content-wrapper flex-center-center flex-column no-flex-grow" style={{border: '1px solid blue'}}>
+            <div className="hero-content-wrapper flex-center-center flex-column no-flex-grow" >
                 <h1 className="centered-text white-text hero-h1 fade-in" style={{maxWidth: '800px'}}>Serving the Treasure Coast with <span style={{color: 'var(--navy)'}}>Integrity</span> </h1>
                 <button className='book-consultation-button fade-in' style={{marginBottom: '25px'}}>Book a Consultation</button>
                 <Link href="#AboutSection" className="flex-center-center no-flex-grow bouncing-arrow fade-in">
